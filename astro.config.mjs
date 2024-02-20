9a4795541bf56d0df2b17d3ffcc249ce33962219
Cima Alfa <119798452+cima-alfa@users.noreply.github.com>
2024-02-20 12:25:13 +0100

    Update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
+import { NEVER } from 'astro/zod';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://portfolio.sjeremy.dev',
+	compressHTML: false,
+	trailingSlash: NEVER
 });
