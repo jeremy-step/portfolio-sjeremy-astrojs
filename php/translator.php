c423bfe57621a83d8d1323002170b15039624c1e
Cima Alfa <119798452+cima-alfa@users.noreply.github.com>
2024-04-28 01:51:19 +0200

    Merge pull request #31 from cima-alfa/dev
    Dev

---
+++ php/translator.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+use Nette\Utils\Strings;
+
+class FormTranslator implements Nette\Localization\Translator {
+	public function __construct(private array $translations) {}
+
+	public function translate(string|\Stringable $message, mixed ...$parameters): string|\Stringable {
+		return $this->formatMessage($this->translations[$message], ...$parameters) ?? $message;
+	}
+
+	private function formatMessage(string $string, ...$parameters): string {
+		$string = Strings::replace($string, '/%(\d+\$[ds]|[ds])/', function (array $matches) use ($parameters): string {
+			static $i = -1;
+
+			$i = (int) $matches[1] ? (int) $matches[1] - 1 : $i + 1;
+
+			return (string) (isset($parameters[$i]) ? $parameters[$i] : $matches[0]);
+		});
+
+		return $string;
+	}
+}
---
+++ php/www/contact-form.php
@@ -0,0 +1,136 @@
+<?php
+
+declare(strict_types=1);
+
+use Nette\Forms\Form;
+use Tracy\Debugger;
+use Nette\Utils\Strings;
+use Nette\Forms\Validator;
+
+header('Access-Control-Allow-Origin: https://portfolio.sjeremy.dev/*');
+header("Content-Type: application/json; charset=utf-8");
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+Debugger::$logDirectory = __DIR__ . '/../log';
+Debugger::$strictMode = true;
+Debugger::enable(Debugger::Production);
+
+require_once __DIR__ . '/../translator.php';
+
+Validator::$messages = [
+	Form::Filled => 'required',
+	Form::Email => 'email'
+];
+
+$locales = [
+	'en',
+	'es'
+];
+
+$locale = isset($_SERVER['HTTP_X_PORTFOLIO_LANG']) && in_array($_SERVER['HTTP_X_PORTFOLIO_LANG'], $locales, true) ? $_SERVER['HTTP_X_PORTFOLIO_LANG'] : $locales[0];
+$translator = new FormTranslator(parse_ini_file(__DIR__ . '/../locale/' . $locale . '.ini'));
+
+$form = new Form;
+$form->allowCrossOrigin();
+$form->setTranslator($translator);
+
+$form->addText('name')
+	->addFilter([Strings::class, 'normalize'])
+	->setRequired()
+	->addFilter([Strings::class, 'capitalize']);
+
+$form->addText('surname')
+	->addFilter([Strings::class, 'normalize'])
+	->setRequired()
+	->addFilter([Strings::class, 'capitalize']);
+
+$form->addEmail('email')
+	->addFilter([Strings::class, 'normalize'])
+	->setRequired();
+
+$form->addTextArea('message')
+	->addFilter([Strings::class, 'normalize'])
+	->setRequired()
+	->addFilter([Strings::class, 'firstUpper']);
+
+$form->addCheckbox('legal')
+	->setRequired();
+
+$form->onError[] = function(Form $form) use ($translator): void {
+	$form->addError($translator->translate('formError'));
+	
+	$errors = [];
+
+	foreach ($form->getControls() as $input => $data) {
+		$errors[$input] = $data->getErrors();
+	}
+
+	echo json_encode(['status' => -1, 'formErrors' => $form->getOwnErrors(), 'inputErrors' => [...$errors]]);
+
+	exit;
+};
+
+$form->onSuccess[] = function(Form $form) use ($translator): void {
+	$mailSettings = parse_ini_file(__DIR__ . '/../settings/mail.ini');
+	$smtpSettings = parse_ini_file(__DIR__ . '/../settings/smtp.local.ini');
+	$nameOwner = $mailSettings['name'];
+	$nameUser = "{$form['name']->getValue()} {$form['surname']->getValue()}";
+	
+	try {
+		$mailOwner = new Nette\Mail\Message;
+		$mailOwner->setFrom($smtpSettings['username'], $nameUser)
+			->addTo($smtpSettings['username'], $nameOwner)
+			->addReplyTo($form['email']->getValue(), $nameUser)
+			->setSubject($translator->translate('subjectOwner', $mailSettings['siteName'], $nameUser))
+			->setHtmlBody($translator->translate(
+				'bodyOwner',
+				$form['name']->getValue(),
+				$form['surname']->getValue(),
+				$form['email']->getValue(),
+				$form['message']->getValue()
+			))
+			->clearHeader('X-Mailer');
+
+		$mailUser = new Nette\Mail\Message;
+		$mailUser->setFrom($smtpSettings['username'], $nameOwner)
+			->addTo($form['email']->getValue(), $nameUser)
+			->addReplyTo($smtpSettings['username'], $nameOwner)
+			->setSubject($translator->translate('subjectUser', $mailSettings['siteName']))
+			->setHtmlBody($translator->translate(
+				'bodyUser',
+				$form['name']->getValue(),
+				$form['surname']->getValue(),
+				$form['email']->getValue(),
+				$form['message']->getValue()
+			))
+			->clearHeader('X-Mailer');
+
+		$mailer = new Nette\Mail\SmtpMailer(
+			host: (string) $smtpSettings['host'],
+			username: (string) $smtpSettings['username'],
+			password: (string) $smtpSettings['password'],
+			encryption: (string) $smtpSettings['encryption'],
+			port: (int) $smtpSettings['port']
+		);
+
+		$mailer->send($mailOwner);
+		$mailer->send($mailUser);
+	} catch (Exception $e) {
+		Debugger::log($e);
+
+		$form->addError($translator->translate('formError'));
+
+		echo json_encode(['status' => -1, 'formErrors' => $form->getOwnErrors(), 'inputErrors' => []]);
+
+		exit;
+	}
+
+	echo json_encode(['status' => 1, 'message' => $translator->translate('formSuccess')]);
+
+	exit;
+};
+
+$form->fireEvents();
+
+header('location: /');
